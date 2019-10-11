b6143b8a76421f7e90f16ff5d43598418bfb0b9c
Wonseo Huh <dnjstj1119@gmail.com>
2019-10-10 22:49:28 -0400

    commit
    second commit

--- src/main/java/leetcode/twosum/TwoSum.java
+++ src/main/java/leetcode/twosum/TwoSum.java
@@ -12,18 +12,36 @@ import java.util.Map;
 // [0,1]
 public class TwoSum {
     public int[] twoSum(int[] numbers, int target) {
-        int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
         for (int i = 0; i < numbers.length; i++) {
+            map.put(numbers[i], i);
 
             if (map.containsKey(target - numbers[i])) {
-                result[1] = i;
-                result[0] = map.get(target - numbers[i]);
-                return result;
+                result[0] = numbers[i];
+                result[1] = map.get(target - numbers[i]);
             }
-
-            map.put(numbers[i], i);
         }
         return result;
+    }
+}
+
+
+
+
+//        int[] result = new int[2];
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < numbers.length; i++) {
+//
+//            if (map.containsKey(target - numbers[i])) {
+//                result[1] = i;
+//                result[0] = map.get(target - numbers[i]);
+//                return result;
+//            }
+//
+//            map.put(numbers[i], i);
+//        }
+//        return result;
     }
 }
